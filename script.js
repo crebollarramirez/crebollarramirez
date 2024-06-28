583e0c7ec8d1acc57dcffad83931d1ff9ab6b151
Chris <christopherrebollar0@icloud.com>
2024-06-28 02:59:50 -0700

    finshing touches

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 function downloadFile() {
-    // Replace 'path/to/your/file.pdf' with the actual path to your file
-    const fileUrl = './extras/crebollarramirez-resume.pdff';
-    const fileName = 'crebollarramirez-resume.pdf';
-
-    fetch(fileUrl)
-      .then(response => response.blob())
-      .then(blob => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        // Set the filename to be downloaded
-        a.download = fileName;
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-      })
-      .catch(error => console.error('Error downloading file:', error));
-  }
+    const link = document.createElement('a');
+    link.href = './extras/crebollarramirez-resume.pdf';
+    link.download = 'crebollarramirez-resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
