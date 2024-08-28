88adfdf087a1e68f98a3927c530ec64919ef3101
Chris <christopherrebollar0@icloud.com>
2024-08-27 21:27:20 -0700

    reset

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export function card(){
+    return (
+        <div id="left-container">
+            <div className="photo-name">
+                <h1 className="left-item">Christopher Rebollar-Ramirez</h1>
+                <h2 className="left-item"><em>Full-Stack Developer</em></h2>
+            </div>
+        </div>
+    );
+};
+
+    // <div id="grid-container">
+    //   <div id="left">
+    //     <div class="left-item grid-item photo-name">
+    //       <img class="photo-left-item" src="images/me.jpeg" alt="me">
+    //       <h1 class="photo-left-item reveal">Christopher Rebollar-Ramirez</h1>
+    //       <h2 class="photo-left-item reveal"><em>Full-Stack Developer</em></h2>
+    //     </div>
+    //     <div class="left-item grid-item socials">
+    //       <a href="https://www.facebook.com/christopher.rebollar.9/" class="fa fa-facebook"></a>
+    //       <a href="https://www.linkedin.com/in/christopher-rebollar-ramirez-1210b5260/  " class="fa fa-linkedin"></a>
+    //       <a href="https://github.com/crebollarramirez" class="fa fa-github"></a>
+    //       <a href="https://www.instagram.com/thereal_stezzy/" class="fa fa-instagram"></a>
+    //     </div>
+    //       <button onclick="downloadFile()" class="left-item button">Download CV</button>
+        
+    //   </div>
+    //   </div>
