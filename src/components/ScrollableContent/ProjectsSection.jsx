f7d54104cda48cfda895618df4cccb23c6ddddd9
Chris <christopherrebollar0@icloud.com>
2024-08-31 01:15:27 -0700

    starting on projects section

--- src/components/ScrollableContent/ProjectsSection.jsx
+++ src/components/ScrollableContent/ProjectsSection.jsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React from "react";
+import "./../../styles/projectSectionStyle.css";
 
-export function ProjectsSection({id}){
-    return (
-        <div className="scroll-item projects-section" id={id}>
-            <h1 className="titleText">Projects</h1>
-        </div>
-    )
+export function ProjectsSection({ id }) {
+  return (
+    <div className="scroll-item projects-section" id={id}>
+      <h1 className="titleText">Projects</h1>
+    </div>
+  );
 }
