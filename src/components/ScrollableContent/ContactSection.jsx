bb9d3f1bdca24dffcf6df16f7b82242aba7198bc
Chris <christopherrebollar0@icloud.com>
2024-09-01 20:32:03 -0700

    update code

--- src/components/ScrollableContent/ContactSection.jsx
+++ src/components/ScrollableContent/ContactSection.jsx
@@ -1,8 +1,10 @@
-import React from "react"
-import './../../styles/contactSectionStyle.css'
+import React from "react";
+import "./../../styles/contactSectionStyle.css";
 
-export function ContactSection({id}){
-    return (<div className="scroll-item contact-section" id={id}>
-        <h1 className="titleText">My Contact</h1>
-    </div>);
+export function ContactSection({ id }) {
+  return (
+    <div className="scroll-item contact-section" id={id}>
+      <h1 className="titleText">My Contact</h1>
+    </div>
+  );
 }
