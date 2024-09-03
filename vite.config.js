4dc3bbd8327be7b73a667e908622e5482e591368
Chris <christopherrebollar0@icloud.com>
2024-09-03 16:10:29 -0700

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/crebollarramirez/',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
 })
