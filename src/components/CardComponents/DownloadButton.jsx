a652def19bd69e3cec96117a0d91375422fea75b
Chris <christopherrebollar0@icloud.com>
2024-09-03 15:51:36 -0700

    added resume

--- src/components/CardComponents/DownloadButton.jsx
+++ src/components/CardComponents/DownloadButton.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import resume from './../../assets/crebollarramirez-resume.pdf'
 
-export function DownloadButton(){
-    return (
-        <button className="card-item DLbutton">Download CV</button>
-    );
+export function DownloadButton() {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = resume; // Update with the actual path to your PDF file
+    link.download = "crebollarramirez-resume.pdf"; // The name the file will have when downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); // Clean up
+  };
+
+  return (
+    <button className="card-item DLbutton" onClick={handleDownload}>
+      Download CV
+    </button>
+  );
 }
