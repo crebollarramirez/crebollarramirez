0a34abe93d69c1bb7d5a30fd1131c34491e0063a
Chris <christopherrebollar0@icloud.com>
2024-09-03 14:29:52 -0700

    seperated number component

--- src/components/ScrollableContent/AboutMeComponents/Numbers.jsx
+++ src/components/ScrollableContent/AboutMeComponents/Numbers.jsx
@@ -1,25 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./../../../styles/numbers.css";
+import { Number } from "./Number";
 
-export function Number({ num, descrition = "" }) {
-  // Default value for descrition
-  function replaceSpaces(input) {
-    return input.split(" ").map((item, index) => (
-      <React.Fragment key={index}>
-        {item}
-        {index < input.split(" ").length - 1 && <br />}
-      </React.Fragment>
-    ));
-  }
-
-  return (
-    <div className="number-item">
-      <h1>{num}</h1>
-      <h2>{replaceSpaces(descrition)}</h2>
-    </div>
-  );
-}
 
 export function Numbers() {
   const [commitCount, setCommitCount] = useState(0);
