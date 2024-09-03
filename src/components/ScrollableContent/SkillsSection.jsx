dcce333a93e194892f4ef5a122c49b8c63532fef
Chris <christopherrebollar0@icloud.com>
2024-09-03 14:44:58 -0700

    added shadows to skills section

--- src/components/ScrollableContent/SkillsSection.jsx
+++ src/components/ScrollableContent/SkillsSection.jsx
@@ -39,14 +39,13 @@ export function SkillsSection({ id }) {
             <h3>Data Science and Machine Learning Libraries:</h3>
             <ul>
               <li>
-                <span className="library">pandas:</span> Proficient in using
-                pandas for data manipulation, including tasks like data
+                <span className="library">pandas:</span> Proficient in data manipulation, including tasks like data
                 cleaning, transformation, and analysis.
               </li>
               <li>
                 <span className="library">scikit-learn:</span> Experienced in
-                applying machine learning algorithms using scikit-learn, such as
-                regression, classification, clustering, <br></br> and model
+                applying machine learning algorithms, such as
+                regression, classification, <br></br> clustering, and model
                 evaluation.
               </li>
             </ul>
@@ -56,14 +55,12 @@ export function SkillsSection({ id }) {
             <h3>Deep Learning Frameworks:</h3>
             <ul>
               <li>
-                <span className="library">PyTorch:</span> Skilled in using
-                PyTorch for deep learning, including building and training
+                <span className="library">PyTorch:</span> Skilled in deep learning, including building and training
                 neural networks, implementing custom models, and performing
                 complex tensor operations.
               </li>
               <li>
-                <span className="library">TensorFlow:</span> Proficient in
-                TensorFlow, with experience in creating and deploying deep
+                <span className="library">TensorFlow:</span> Experience in creating and deploying deep
                 learning models, leveraging TensorFlow's extensive ecosystem for
                 model building, training, and optimization.
               </li>
@@ -75,7 +72,7 @@ export function SkillsSection({ id }) {
             <ul>
               <li>
                 <span className="library2">Keras:</span> Adept at building
-                neural networks using Keras, with experience in designing and
+                neural networks, with experience in designing and
                 training models for tasks such as image classification, natural
                 language processing, and more.
               </li>
@@ -92,8 +89,7 @@ export function SkillsSection({ id }) {
                 effectively communicate data insights.
               </li>
               <li>
-                <span className="library2">Seaborn:</span> Skilled in using
-                Seaborn for statistical data visualization, including creating
+                <span className="library2">Seaborn:</span> Skilled in creating
                 advanced visualizations like heatmaps, pair plots, and violin
                 plots.
               </li>
