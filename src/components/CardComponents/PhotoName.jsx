8940bde38ae2e8d6f81d1ac8fabc761a22cd7b1b
Chris <christopherrebollar0@icloud.com>
2024-09-10 01:27:34 -0700

    fixed photo-name support for mobile

--- src/components/CardComponents/PhotoName.jsx
+++ src/components/CardComponents/PhotoName.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import me from './../../assets/me.jpeg'
+import me from "./../../assets/me.jpeg";
 
-export function PhotoName(){
-    return (
-        <div className="photo-name">  
-            <div className="imageContainer">
-                <img className="photo-left-item" src={me} alt="me"/>
-            </div>
-            <h1 className="photo-left-item">Christopher Rebollar-Ramirez</h1>
-            <h2 className="photo-left-item"><em>Computer Science Student</em></h2>`
-        </div>
-    );
-
+export function PhotoName() {
+  return (
+    <div className="photo-name">
+      <div className="imageContainer">
+        <img className="photo-left-item" src={me} alt="me" />
+      </div>
+      <div className="name-ocu">
+        <h1 className="photo-left-item">Christopher Rebollar-Ramirez</h1>
+        <h2 className="photo-left-item">
+          <em>Computer Science Student</em>
+        </h2>
+      </div>
+    </div>
+  );
 }
